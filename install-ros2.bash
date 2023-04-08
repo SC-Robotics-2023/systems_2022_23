d8f4261e70ac440a14e8a7f86ecebf8ab1b3df69
cam <camwolff02@gmail.com>
2023-04-08 16:53:48 -0700

    added launch files and setup bash scripts

---
+++ install-ros2.bash
@@ -0,0 +1,42 @@
+# Installs ros2 humble on linux
+locale  # check for UTF-8
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+locale  # verify settings
+
+
+sudo apt install software-properties-common -y
+sudo add-apt-repository universe
+
+
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+
+sudo apt update
+sudo apt-get upgrade -y
+
+
+sudo apt install python3-colcon-common-extensions -y
+sudo apt install ros-humble-desktop -y
+sudo apt install python3-rosdep2 -y
+
+
+# Set up ROS environment
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+echo "export ROS_DOMAIN_ID=5" >> ~/.bashrc
+
+# Set up Colcon features
+echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
+echo "export _colcon_cd_root=/opt/ros/humble/" >> ~/.bashrc
+echo "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" >> ~/.bashrc
+
+# Install git
+sudo apt install git -y
---
+++ systems_setup.bash
@@ -0,0 +1,8 @@
+sudo apt update && sudo apt upgrade -y
+
+# Install Gazebo
+sudo apt install ros-humble-gazebo-ros-pkgs -y
+
+# Install xacro and joint state publisher GUI
+sudo apt install ros-humble-xacro -y
+sudo apt install ros-humble-joint-state-publisher-gui -y
