82a45051203ed356e6cf5fc107c7e1be323bed7a
cam <camwolff02@gmail.com>
2023-04-15 19:06:36 -0700

    added ros2_control support with keyboard

--- systems_setup.bash
+++ systems_setup.bash
@@ -6,3 +6,13 @@ sudo apt install ros-humble-gazebo-ros-pkgs -y
 # Install xacro and joint state publisher GUI
 sudo apt install ros-humble-xacro -y
 sudo apt install ros-humble-joint-state-publisher-gui -y
+
+# Install ros2 control
+sudo apt install ros-humble-ros2-control ros-humble-ros2-controllers ros-humble-gazebo-ros2-control -y
+
+
+
+
+
+# Install any missing dependencies
+rosdep install --from-paths src --ignore-src -r -y
